4eb5541af7480f404c1491247740513269cd485d
Jihun Lee <zeroion83@gmail.com>
2023-09-09 02:49:42 +0900

    feat: Added profile listing feature

--- src/components/ProfileList.tsx
+++ src/components/ProfileList.tsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, List, ListItem } from "@mui/material";
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from "../database/db";
 
 export default function ProfileList() {
+  const profiles = useLiveQuery(() => db.profiles.toArray(), []);
+
   return (
     <div>
       <h1>프로필 목록</h1>
       <Link to="/avatar/new">
         <Button variant="text">새로운 프로필</Button>
       </Link>
+      <List>
+        {profiles?.map((profile) => (
+          <ListItem key={profile.id}>
+            <Link to={`/avatar/${profile.id}`}>
+              <Button variant="text">{profile.name}</Button>
+            </Link>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
